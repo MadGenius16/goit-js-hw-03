7a4d050c127bd5b955231e1b0f105829d3eda214
MadGenius16 <aleksandrco777@gmail.com>
2024-01-27 16:53:31 +0200

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,31 +1,20 @@
 
 function filterArray(numbers, value) {
   let newArr=[]
-  for(const number of numbers) {
-    if(numbers[i>value) {
-      newArr.push(numbers[i])
-    }
-  }
-  return newArr
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-
-// function filterArray(numbers, value)
-// const numberArr = [];
-// for(const number of numbers) {
-//   if(number[i]>value) {
-//    return numbers
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
